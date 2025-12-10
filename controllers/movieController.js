b5011e0946ceda92b8258afd9b98722dcf6b8c5e
giannivitale <giavitale78@gmail.com>
2025-12-10 18:09:10 +0100

    index function to fetch and return all movies from the database

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,12 +1,20 @@
 const connection = require('../database/connection')
 
 const index = (req, res) => {
-    res.send('Show all movies here')
+
+    const sql = 'SELECT * FROM movies'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: true, message: err.message })
+        //console.log(results);
+        res.json(results)
+    })
 }
 
 
 const show = (req, res) => {
     res.send('Show me the movie with id ' + req.params.id)
+
+    
 }
 
 
