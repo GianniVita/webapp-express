1e3d5cb746ae27dc203f7d5d717dfcd1ac052853
giannivitale <giavitale78@gmail.com>
2025-12-10 17:42:47 +0100

    fix: correct express dependency name and update version

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2')
 const connection = mysql2.createConnection({
     
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_DATABASE
 
 })
 
